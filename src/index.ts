9d79c805967c97f13b03d130ff3d57754241d9bf
Jason Sun <jason@adanomad.com>
2026-02-07 14:48:58 -0500

    release: v0.2.0

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("ace")
   .description("AceTeam CLI - Run AI workflows locally")
-  .version("0.1.0");
+  .version("0.2.0");
 
 program.addCommand(initCommand);
 program.addCommand(workflowCommand);
